03e5174c27ac27b932f6b214845b44902f5eb2e3
alvator92 <alvator@list.ru>
2023-09-22 23:26:13 +0300

    init rain sensor

---
+++ rain/src/main/java/ru/test/rain/RainSensorApp.java
@@ -0,0 +1,13 @@
+package ru.test.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainSensorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainSensorApp.class, args);
+    }
+
+}
